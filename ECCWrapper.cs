976ce2e9ae45d564e1ea097e69f8091046ae9db3
MrQugen <mrqugen@gmail.com>
2023-11-21 19:04:25 +0200

    Initial commit

---
+++ ECCWrapper.cs
@@ -0,0 +1,91 @@
+﻿using Org.BouncyCastle.Asn1.X9;
+using Org.BouncyCastle.Crypto.EC;
+using Org.BouncyCastle.Math;
+using Org.BouncyCastle.Math.EC;
+using Org.BouncyCastle.Utilities.Encoders;
+
+namespace WrapperForUsingAWorkingLibraryWithAlgebraOnEllipticCurves;
+
+public class ECCWrapper
+{
+    private readonly X9ECParameters CurveParams;
+
+    public ECCWrapper(string curveName)
+    {
+        CurveParams = CustomNamedCurves.GetByName(curveName) ?? throw new ArgumentException("Invalid curve name");
+    }
+
+    // Отримання G-генератора
+    public ECPoint BasePointGGet()
+    {
+        return CurveParams.G.Normalize();
+    }
+
+    // Створення точки EC
+    public ECPoint ECPointGen(BigInteger x, BigInteger y)
+    {
+        ECCurve curve = CurveParams.Curve;
+        return curve.CreatePoint(x, y);
+    }
+
+    // Перевірка, чи належить точка кривій
+    public bool IsOnCurveCheck(ECPoint point)
+    {
+        ECCurve curve = CurveParams.Curve;
+        return point.IsValid() && point.Curve.Equals(curve);
+    }
+
+    // Додавання двох точок на кривій
+    public ECPoint AddECPoints(ECPoint a, ECPoint b)
+    {
+        return a.Add(b).Normalize();
+    }
+
+    // Подвоєння точки на кривій
+    public ECPoint DoubleECPoints(ECPoint a)
+    {
+        return a.Twice().Normalize();
+    }
+
+    // Множення точки на скаляр
+    public ECPoint ScalarMult(BigInteger k, ECPoint a)
+    {
+        return a.Multiply(k).Normalize();
+    }
+
+    // Серіалізація точки в рядок
+    public string ECPointToString(ECPoint point)
+    {
+        byte[] encodedPoint = point.GetEncoded();
+        return Hex.ToHexString(encodedPoint);
+    }
+
+    // Десеріалізація точки з рядка
+    public ECPoint StringToECPoint(string s)
+    {
+        ECCurve curve = CurveParams.Curve;
+        byte[] encodedPoint = Hex.Decode(s);
+        return curve.DecodePoint(encodedPoint).Normalize();
+    }
+
+    // Порівняння двох точок
+    public bool AreEqual(ECPoint a, ECPoint b)
+    {
+        return a.Normalize().Equals(b.Normalize());
+    }
+
+    // Порівняння двох точок за їхніми строковими представленнями
+    public bool AreEqual(string s1, string s2)
+    {
+        ECPoint point1 = StringToECPoint(s1);
+        ECPoint point2 = StringToECPoint(s2);
+
+        return AreEqual(point1, point2);
+    }
+
+    // Виведення точки на екран
+    public void PrintECPoint(ECPoint point)
+    {
+        Console.WriteLine(point.Normalize());
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,66 @@
+﻿using Org.BouncyCastle.Math;
+using Org.BouncyCastle.Math.EC;
+
+namespace WrapperForUsingAWorkingLibraryWithAlgebraOnEllipticCurves;
+
+public static class Program
+{
+    public static void Main()
+    {
+        ECCWrapper eccWrapper = new ECCWrapper("secp256k1");
+
+        // Отримання G-генератора
+        ECPoint G = eccWrapper.BasePointGGet();
+        Console.WriteLine("G: " + eccWrapper.ECPointToString(G));
+
+        // Генерація випадкових чисел k та d
+        BigInteger k = SetRandom(256);
+        BigInteger d = SetRandom(256);
+
+        // Створення точок H1 та H2
+        ECPoint H1 = eccWrapper.ScalarMult(d, G);
+        ECPoint H2 = eccWrapper.ScalarMult(k, H1);
+
+        Console.WriteLine("H1: " + eccWrapper.ECPointToString(H1));
+        Console.WriteLine("H2: " + eccWrapper.ECPointToString(H2));
+
+        // Створення точок H3 та H4
+        ECPoint H3 = eccWrapper.ScalarMult(k, G);
+        ECPoint H4 = eccWrapper.ScalarMult(d, H3);
+
+        Console.WriteLine("H3: " + eccWrapper.ECPointToString(H3));
+        Console.WriteLine("H4: " + eccWrapper.ECPointToString(H4));
+
+        // Перевірка рівності H2 та H4
+        bool result = eccWrapper.AreEqual(H2, H4);
+        Console.WriteLine("Are H2 and H4 equal? " + result);
+
+        // Демонстрація роботи інших методів
+        ECPoint sum = eccWrapper.AddECPoints(H2, H4);
+        Console.WriteLine("Sum of H2 and H4: " + eccWrapper.ECPointToString(sum));
+
+        ECPoint doubled = eccWrapper.DoubleECPoints(H1);
+        Console.WriteLine("Double H1: " + eccWrapper.ECPointToString(doubled));
+
+        // Виведення рядка точки на екран
+        string serializedPoint = eccWrapper.ECPointToString(G);
+        Console.WriteLine("Serialized G: " + serializedPoint);
+
+        // Десеріалізація точки з рядка
+        ECPoint deserializedPoint = eccWrapper.StringToECPoint(serializedPoint);
+        Console.WriteLine("Deserialized Point: " + eccWrapper.ECPointToString(deserializedPoint));
+
+        // Виведення точки на екран
+        eccWrapper.PrintECPoint(G);
+    }
+
+    // Генерація випадкового числа заданої бітової довжини
+    public static BigInteger SetRandom(int bitLength)
+    {
+        Random random = new Random();
+        byte[] randomBytes = new byte[(bitLength + 7) / 8];
+        random.NextBytes(randomBytes);
+        randomBytes[randomBytes.Length - 1] |= (byte)(1 << ((bitLength - 1) % 8));
+        return new BigInteger(1, randomBytes);
+    }
+}
